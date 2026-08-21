36d9358201905f76f6782f04985fd4a8c6d8acbe
SanSanich2503 <SanSanich25031997@yandex.ru>
2026-08-21 22:23:43 +0500

    Обновленный
    Обновленный

--- Проекты/C#/Веб-приложения/InternetShopApp/Services/Services/RabbitMqService.cs
+++ Проекты/C#/Веб-приложения/InternetShopApp/Services/Services/RabbitMqService.cs
@@ -1,6 +1,35 @@
-﻿namespace Services.Services;
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace Services.Services;
 
 public class RabbitMqService
 {
-    
+    /*public void SendMessage(object obj)
+    {
+        var message = JsonSerializer.Serialize(obj);
+        SendMessage(message);
+    }
+
+    private void SendMessage(string message)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "MyQueue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                routingKey: "MyQueue",
+                basicProperties: null,
+                body: body);
+        }
+    }*/
 }
